e423521a38433f4fd719894b7194441553c35863
Mbrelot <mael.brelot@lecole-ldlc.com>
2020-11-26 13:04:11 +0100

    Voilà ce que j'ai eu le temps de faire pendant cette heure. Bonne journée

---
+++ Assets/MovePlateforme.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlateforme : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 direction = new Vector3(0f, 0f, 1f);
+        float speed = 2f;
+
+  
+
+
+        if (transform.position.z <= 4.5f)
+        {
+          direction = new Vector3(0f, 0f, -1f);
+        }
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+
+        rigidbody.MovePosition(newPosition);
+    }
+}
---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject doorObject;
+    float timer;
+    float timer2;
+    private void OnTriggerEnter(Collider other)
+    {
+        doorObject.SetActive(false);
+        timer = 5.0f;
+        timer2= 0.0f;
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        ///doorObject.SetActive(false);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        doorObject.SetActive(true);
+    }
+    private void Update()
+    {
+        if (timer2 > timer)
+        {
+            doorObject.SetActive(true);
+        }
+        timer2 += Time.deltaTime;
+    }
+}
